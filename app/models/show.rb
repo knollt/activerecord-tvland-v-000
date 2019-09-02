25a5eb12bf444e4624d6c47f068319e25070677d
Tim Knoll <timothy.knoll@gmail.com>
2019-09-02 15:24:05 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  belongs_to :network
+  has_many :actors, through: :characters
   
+  def actors_list
+    # Show.actors.full_name.length
+    Show.includes(name)
+    # Show.joins("first_name","last_name").length
+  end  
+  
+  #     Show.order(name: :asc)
 end
